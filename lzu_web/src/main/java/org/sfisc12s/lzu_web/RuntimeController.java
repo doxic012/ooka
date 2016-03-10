e713ea70e6eb65e75f590510db81876e441a5bf5
doxic012 <doxic012@gmail.com>
2016-03-11 00:23:54 +0100

    componentfactory entfernt. lzu_web angepasst

--- lzu_web/src/main/java/org/sfisc12s/lzu_web/RuntimeController.java
+++ lzu_web/src/main/java/org/sfisc12s/lzu_web/RuntimeController.java
@@ -1,37 +1,63 @@
 package org.sfisc12s.lzu_web;
 
+import org.ooka.sfisc12s.runtime.environment.RuntimeEnvironment;
+import org.ooka.sfisc12s.runtime.environment.component.impl.ReferenceComponent;
+import org.ooka.sfisc12s.runtime.environment.component.state.exception.StateException;
+import org.ooka.sfisc12s.runtime.util.Logger.Impl.LoggerFactory;
+import org.ooka.sfisc12s.runtime.util.Logger.Logger;
 import org.primefaces.event.FileUploadEvent;
-import org.sfisc12s.lzu_web.util.JsfUtil;
-import org.sfisc12s.lzu_web.util.PaginationHelper;
-import org.sfisc12s.test.NewEntity;
-import org.sfisc12s.test.NewEntityFacade;
+import org.primefaces.model.UploadedFile;
 
-import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
-import javax.faces.model.DataModel;
-import javax.faces.model.ListDataModel;
-import javax.faces.model.SelectItem;
 import javax.inject.Named;
-import java.io.Serializable;
-import java.util.ResourceBundle;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
 
 @Named("runtime")
 @SessionScoped
 public class RuntimeController implements Serializable {
 
+    private static Logger log = LoggerFactory.getRuntimeLogger(RuntimeController.class);
+
+    private RuntimeEnvironment re = RuntimeEnvironment.getInstance();
+
     public RuntimeController() {
     }
 
     public void loadLibrary(FileUploadEvent event) {
-        test = "load lib: "+event.getFile().getFileName();
+        UploadedFile file = event.getFile();
+        String fileName = file.getFileName();
+        try {
+            File f = new File("\\libs\\" + fileName);
+
+            // TODO: Popup oder alert?
+            if (f.exists()) {
+                test = "file exists";
+                return;
+            }
+            OutputStream outputStream = new FileOutputStream(f);
+            InputStream inputStream = file.getInputstream();
+
+            int read;
+            byte[] bytes = new byte[1024];
+
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+
+            re.getOrAdd(new ReferenceComponent(fileName, f.toURI().toURL(), "no scope web")).load();
+
+            test = "load lib: " + event.getFile().getFileName();
+        } catch (IOException | StateException e) {
+            e.printStackTrace();
+            log.error(e, "Error while writing uploaded file '%s' to file-system", file.getFileName());
+            test = "Error while writing uploaded file '%s' to file-system\", file.getFileName()";
+        }
     }
+
     public void addComponent(FileUploadEvent event) {
-        test = "add cmp: "+event.getFile().getFileName();
+        test = "add cmp: " + event.getFile().getFileName();
     }
 
     private String test = "test";
